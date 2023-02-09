ec48ca9d72f6ba39190759534952d5a69957d96c
aa <71987344+flxflai@users.noreply.github.com>
2023-02-08 16:16:33 -0800

    Initial Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="blue" />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box />
+      </Canvas>
+    </div>
+  )
+}
