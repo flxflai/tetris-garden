6aa85722e02999190340261ffe6a0548c28060d5
aa <71987344+flxflai@users.noreply.github.com>
2023-02-13 15:53:47 -0800

    Adding .gitignore

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,9 @@ function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame((state, delta) => (ref.current.rotation.y -= 0.01))
+  useFrame((state, delta) => (ref.current.rotation.z += 0.01))  // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
